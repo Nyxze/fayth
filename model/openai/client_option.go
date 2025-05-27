892bd5d1fd0459e5f6a2d0157c3ef15358048be8
Nyxze <alexy.manach@monamipoto.com>
2025-05-27 12:11:15 +0200

    feat(openai): Renamed call_options to client_options

---
+++ model/openai/client_option.go
@@ -0,0 +1,46 @@
+package openai
+
+import (
+	"nyxze/fayth/model"
+	"nyxze/fayth/model/openai/internal"
+)
+
+type ClientOption func(*clientOptions) error
+
+// Wrapper around  [internal.CallOption] and [model.ModelOption]
+type clientOptions struct {
+	modelOpts    []model.ModelOption
+	internalOpts []internal.CallOption
+}
+
+// WithAPIKey sets the API key to authenticate requests.
+func WithAPIKey(key string) ClientOption {
+	return func(opts *clientOptions) error {
+		opts.internalOpts = append(opts.internalOpts, internal.WithAPIKey(key))
+		return nil
+	}
+}
+
+// WithBaseURL sets a custom base URL for the API.
+func WithBaseURL(base string) ClientOption {
+	return func(opts *clientOptions) error {
+		opts.internalOpts = append(opts.internalOpts, internal.WithBaseURL(base))
+		return nil
+	}
+}
+
+// WithModel sets default model to use for generation.
+func WithModel(name string) ClientOption {
+	return func(opts *clientOptions) error {
+		opts.modelOpts = append(opts.modelOpts, model.WithModel(name))
+		return nil
+	}
+}
+
+// WithOrganization sets the organization ID or name.
+func WithOrganization(org string) ClientOption {
+	return func(opts *clientOptions) error {
+		opts.internalOpts = append(opts.internalOpts, internal.WithOrganization(org))
+		return nil
+	}
+}
